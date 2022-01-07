4b83a4672f78025ee7b0397d535a50bc7bfdcd74
reubie <r.nguyo27@gmail.com>
2022-01-08 00:12:01 +0300

    set clicklisteners for both signin and signup texts

--- app/src/main/java/com/example/niaje/activities/SignUpActivity.java
+++ app/src/main/java/com/example/niaje/activities/SignUpActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.niaje.R;
+import com.example.niaje.databinding.ActivitySignInBinding;
+import com.example.niaje.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
     }
 }

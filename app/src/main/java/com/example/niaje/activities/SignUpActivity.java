9ee3b886cbc818a1d930afb039e6caff53d9812b
reubie <r.nguyo27@gmail.com>
2022-01-07 19:42:35 +0300

    creation of signin and signup activities

---
+++ app/src/main/java/com/example/niaje/activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.niaje.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.niaje.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
